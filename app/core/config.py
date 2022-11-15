92605e5f676c399c1659a144f082f7e2661fd5c6
pozarnik <pozarnik@tut.by>
2022-11-15 22:15:57 +0300

    Add default DB

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class Setting(BaseModel):
     app_title: str = 'Благотворительный фонд QRKot'
     app_description: str = 'Сервис для поддержки нуждающихся'
-    database_url: str
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     hash_gen_key: str = 'Secret'
 
     class Config:
