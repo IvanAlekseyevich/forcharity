38fbfdc7d52069da81af0e07c79888d097995781
pozarnik <pozarnik@tut.by>
2022-11-20 00:45:25 +0300

    Update endpoints

--- app/api/endpoints/charity.py
+++ app/api/endpoints/charity.py
@@ -3,8 +3,12 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.validators import (
+    check_name_duplicate, check_charity_exists, check_charity_close, check_charity_donations
+)
 from app.core.db import get_async_session
 from app.core.user import current_superuser
+from app.crud.charity import charity_crud
 from app.schemas.charity import CharityProjectDB, CharityProjectCreate, CharityProjectUpdate
 
 router = APIRouter()
@@ -17,6 +21,7 @@ router = APIRouter()
 async def get_all_charity(
         session: AsyncSession = Depends(get_async_session),
 ):
+    """Получает список всех проектов"""
     all_charity = await charity_crud.get_multi(session)  # Запрос всех проектов
     return all_charity
 
@@ -30,7 +35,10 @@ async def create_new_charity(
         charity=CharityProjectCreate,  # Схема создания проекта
         session: AsyncSession = Depends(get_async_session),
 ):
-    """Только для суперюзеров."""
+    """
+    Только для суперюзеров.
+    Создает благотворительный проект.
+    """
     await check_name_duplicate(charity.name, session)  # Проверка дубликата названия
     new_charity = await charity_crud.create(charity, session)  # Создание проекта
     return new_charity
@@ -43,14 +51,18 @@ async def create_new_charity(
 )
 async def partially_update_charity(
         charity_id=int,  # id проекта
-        charity_obj=CharityProjectUpdate,  # Схема обновления проекта с проверкой на пустое имя
+        charity_obj=CharityProjectUpdate,  # Схема обновления проекта
         session: AsyncSession = Depends(get_async_session),
 ):
-    """Только для суперюзеров."""
+    """
+    Только для суперюзеров.
+    Закрытый проект нельзя редактировать, также нельзя установить требуемую сумму меньше уже вложенной.
+    """
     charity = await check_charity_exists(charity_id, session)  # Проверка существования проекта
     if charity_obj.name is not None:
+        await check_charity_close(charity)  # Проверка закрыт ли проект
+        await check_charity_donations(charity)  # Проверка задоначенной суммы в проект
         await check_name_duplicate(charity_obj.name, session)  # Проверка на дубликат имени
-        await check_donations(charity, charity_obj.full_amount, session)  # Проверка задоначенной суммы в проект
     charity = await charity_crud.update(charity, charity_obj, session)
     return charity
 
@@ -64,8 +76,11 @@ async def remove_charity(
         charity_id=int,  # id удаляемого проекта
         session: AsyncSession = Depends(get_async_session),
 ):
-    """Только для суперюзеров."""
+    """
+    Только для суперюзеров.
+    Удаляет проект. Нельзя удалить проект, в который уже были инвестированы средства, его можно только закрыть.
+    """
     charity = await check_charity_exist(charity_id, session)  # Проверка существования проекта
-    await check_donations(charity, session)  # Проверка доната проекта
+    await check_charity_donations(charity)  # Проверка доната проекта
     charity = await charity_crud.remove(charity, session)
     return charity
--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -19,6 +19,10 @@ router = APIRouter()
 async def get_all_donations(
         session: AsyncSession = Depends(get_async_session),
 ):
+    """
+    Только для суперюзеров.
+    Получает список всех пожертвований.
+    """
     all_donations = await donations_crud.get_multi(session)
     return all_donations
 
@@ -33,6 +37,7 @@ async def create_new_donation(
         session: AsyncSession = Depends(get_async_session),
         user: User = Depends(current_user),
 ):
+    """Сделать пожертвование."""
     new_donation = donation_crud.create(donation, session, user)
     return new_donation
 
@@ -46,5 +51,6 @@ async def get_my_donations(
         session: AsyncSession = Depends(get_async_session),
         user: User = Depends(current_user),
 ):
+    """Получить список моих пожертвований."""
     donations = await donations_crud.get_by_user(user, session)
     return donations
