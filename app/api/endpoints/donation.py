482aae44d627324ddfa33c1620b1abad2a55252b
pozarnik <pozarnik@tut.by>
2022-11-20 20:33:11 +0300

    Fix crud name

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
+from app.crud.donation import donation_crud
 from app.core.user import current_superuser, current_user
 from app.schemas.donation import DonationAdminDB, DonationDB, DonationCreate
 
@@ -23,7 +24,7 @@ async def get_all_donations(
     Только для суперюзеров.
     Получает список всех пожертвований.
     """
-    all_donations = await donations_crud.get_multi(session)
+    all_donations = await donation_crud.get_multi(session)
     return all_donations
 
 
@@ -52,5 +53,5 @@ async def get_my_donations(
         user: User = Depends(current_user),
 ):
     """Получить список моих пожертвований."""
-    donations = await donations_crud.get_by_user(user, session)
+    donations = await donation_crud.get_by_user(user, session)
     return donations
