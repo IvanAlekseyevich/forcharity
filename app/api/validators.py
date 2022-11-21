1de31ee24de94c0eb0a9ecdf87de12813d119f17
pozarnik <pozarnik@tut.by>
2022-11-21 06:23:16 +0300

    Rename, update check_project_donations

--- app/api/validators.py
+++ app/api/validators.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud.charity import charity_crud
+from app.crud.charity import charity_project_crud
 from app.models import CharityProject
 
 
@@ -9,7 +9,7 @@ async def check_name_duplicate(
         charity_name: str,
         session: AsyncSession,
 ) -> None:
-    charity_id = await charity_crud.get_charity_id_by_name(charity_name, session)
+    charity_id = await charity_project_crud.get_charity_id_by_name(charity_name, session)
     if charity_id is not None:
         raise HTTPException(
             status_code=422,
@@ -17,33 +17,39 @@ async def check_name_duplicate(
         )
 
 
-async def check_charity_exists(
-        charity_id: int,
+async def check_project_exists(
+        project_id: int,
         session: AsyncSession,
 ) -> CharityProject:
-    charity = await charity_crud.get(charity_id, session)
-    if charity is None:
+    project = await charity_project_crud.get(project_id, session)
+    if project is None:
         raise HTTPException(
             status_code=404,
             detail='Проект не найден!'
         )
-    return charity
+    return project
 
 
-async def check_charity_close(
-        charity: CharityProject,
+async def check_project_close(
+        project: CharityProject,
 ) -> None:
-    if charity.fully_invested is False:
+    if project.fully_invested is True:
         raise HTTPException(
             status_code=422,
             detail='Проект закрыт!'
         )
 
 
-async def check_charity_donations(
-        charity: CharityProject,
+async def check_project_donations(
+        project: CharityProject,
+        new_amount: int = None
 ) -> None:
-    if charity.invested_amount != 0:
+    if new_amount is not None and project.invested_amount > new_amount:
+        raise HTTPException(
+            status_code=422,
+            detail='Нельзя установить сумму, ниже уже вложенной!'
+        )
+    if new_amount is None and project.invested_amount != 0:
         raise HTTPException(
             status_code=422,
             detail='В проект уже внесены средства!'
