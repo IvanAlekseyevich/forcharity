dbba28b28cbbf0e36f2f3c3d92172af2f19e5129
pozarnik <pozarnik@tut.by>
2022-11-21 06:21:00 +0300

    Rename func

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 from app.core.user import current_superuser, current_user
 from app.crud.donation import donation_crud
+from app.models.user import User
 from app.schemas.donation import DonationAdminDB, DonationDB, DonationCreate
 from app.services.investing import invest_donation
 
@@ -22,7 +23,7 @@ async def get_all_donations(
         session: AsyncSession = Depends(get_async_session),
 ):
     """
-    Только для суперюзеров.
+    Только для суперюзеров.\n
     Получает список всех пожертвований.
     """
     all_donations = await donation_crud.get_multi(session)
@@ -41,7 +42,7 @@ async def create_new_donation(
 ):
     """Сделать пожертвование."""
     new_donation = await donation_crud.create(donation, session, user)
-    new_donation = await invest_donation(new_donation, session)
+    # new_donation = await invest_donation(new_donation, session)
     return new_donation
 
 
@@ -50,7 +51,7 @@ async def create_new_donation(
     response_model=List[DonationDB],
     response_model_exclude_none=True,
 )
-async def get_my_donations(
+async def get_user_donations(
         session: AsyncSession = Depends(get_async_session),
         user: User = Depends(current_user),
 ):
