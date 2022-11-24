818abb4fbb340a5f6df31bded6249838dbe2b8d9
pozarnik <pozarnik@tut.by>
2022-11-24 06:36:44 +0300

    Rename app -> start_app

--- app/main.py
+++ app/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 from app.api.routers import main_router
 from app.core.settings import settings
 
-app = FastAPI(title=settings.app_title, description=settings.app_description)
-app.include_router(main_router)
+start_app = FastAPI(title=settings.app_title, description=settings.app_description)
+start_app.include_router(main_router)
