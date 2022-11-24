71b1b5006502f648d1c88f209af1aaef39f00a60
pozarnik <pozarnik@tut.by>
2022-11-24 05:18:44 +0300

    Rename config.py -> settings.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.routers import main_router
-from app.core.config import settings
+from app.core.settings import settings
 
 app = FastAPI(title=settings.app_title, description=settings.app_description)
 app.include_router(main_router)
