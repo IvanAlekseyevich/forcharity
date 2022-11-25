4d082f72c34ad0fc1b3f72b605ecb9863ea47a63
pozarnik <pozarnik@tut.by>
2022-11-25 06:11:07 +0300

    Change routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-from app.api.routers import main_router
+from app.api.endpoints import charity_project_router, donation_router, user_router
 from app.core.settings import settings
 
 start_app = FastAPI(title=settings.app_title, description=settings.app_description)
-start_app.include_router(main_router)
+start_app.include_router(charity_project_router)
+start_app.include_router(donation_router)
+start_app.include_router(user_router)
