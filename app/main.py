d2673f899c662021d406849469fd8a340d106598
pozarnik <pozarnik@tut.by>
2022-11-25 06:18:16 +0300

    Rename endpoints -> routers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.api.endpoints import charity_project_router, donation_router, user_router
+from app.api.routers import charity_project_router, donation_router, user_router
 from app.core.settings import settings
 
 start_app = FastAPI(title=settings.app_title, description=settings.app_description)
