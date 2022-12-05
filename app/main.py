1bbf2cd87552e3d05d7f3a2b40b6d33a7f339640
pozarnik <pozarnik@tut.by>
2022-12-05 08:00:19 +0300

    Rename start_app -> app

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.api.routers import charity_project_router, donation_router, user_router
 from app.core.settings import settings
 
-start_app = FastAPI(title=settings.app_title, description=settings.app_description)
-start_app.include_router(charity_project_router)
-start_app.include_router(donation_router)
-start_app.include_router(user_router)
+app = FastAPI(title=settings.app_title, description=settings.app_description)
+app.include_router(charity_project_router)
+app.include_router(donation_router)
+app.include_router(user_router)
